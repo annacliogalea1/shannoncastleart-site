74fbffd15b00e5566991b6c3bd7ea76e35f796ca
annacliogalea1 <cliogalea@gmail.com>
2025-08-07 20:24:43 -0600

    Homepage layout tweaks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Preload all banner images
-const preloadImages = [
+const bannerImages = [
   "images/bike-feature-5.jpg",
   "images/bike-feature-7.jpg",
   "images/GLASS 6.jpg",
@@ -7,10 +7,13 @@ const preloadImages = [
   "images/moto 2.png"
 ];
 
-preloadImages.forEach((src) => {
-  const img = new Image();
-  img.src = src;
-});
+let current = 0;
+const banner = document.getElementById('banner-image');
+
+setInterval(() => {
+  current = (current + 1) % bannerImages.length;
+  banner.src = bannerImages[current];
+}, 4000);
 
 const toggleBtn = document.getElementById('menu-toggle');
 const fullscreenMenu = document.getElementById('fullscreen-menu');
@@ -67,74 +70,3 @@ contactForm.addEventListener('submit', (e) => {
     }, 600);
   }, 5000);
 });
-
-const slider = document.getElementById('banner-slider');
-const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.dot');
-const next = document.querySelector('.arrow.next');
-const prev = document.querySelector('.arrow.prev');
-let index = 0;
-let interval = setInterval(nextSlide, 5000);
-
-function updateSlide(position) {
-  slider.style.transform = 'translateX(' + (-100 * position) + '%)';
-  document.querySelector('.dot.active').classList.remove('active');
-  dots[position].classList.add('active');
-}
-
-function nextSlide() {
-  index = (index + 1) % slides.length;
-  updateSlide(index);
-}
-
-function prevSlide() {
-  index = (index - 1 + slides.length) % slides.length;
-  updateSlide(index);
-}
-
-next.addEventListener('click', () => {
-  nextSlide();
-  resetInterval();
-});
-
-prev.addEventListener('click', () => {
-  prevSlide();
-  resetInterval();
-});
-
-dots.forEach((dot, i) => {
-  dot.addEventListener('click', () => {
-    index = i;
-    updateSlide(index);
-    resetInterval();
-  });
-});
-
-function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(nextSlide, 5000);
-}
-
-// Touch/Swipe Support
-let startX = 0;
-let endX = 0;
-
-slider.addEventListener('touchstart', (e) => {
-  startX = e.touches[0].clientX;
-}, false);
-
-slider.addEventListener('touchend', (e) => {
-  endX = e.changedTouches[0].clientX;
-  handleSwipe();
-}, false);
-
-function handleSwipe() {
-  const threshold = 50; // min distance for swipe
-  if (endX - startX > threshold) {
-    prevSlide();
-    resetInterval();
-  } else if (startX - endX > threshold) {
-    nextSlide();
-    resetInterval();
-  }
-}
